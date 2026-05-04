b561320a5bf1596d8c9af82f5ddc0d149f62f332
emil-yan <155638261+emil-yan@users.noreply.github.com>
2026-05-05 01:43:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function validatePassword(password) {
     if (password.length < 8) {
-        return "Password must be at least 8 characters.";
+        return "Password must be at least 8 characters long. At least one lowercase letter, one UPPERCASE letter, and one number.";
     }
 
     if (!/[a-z]/.test(password)) {
-        return "Password must contain at least one lowercase letter.";
+        return "Password must be at least 8 characters long. At least one lowercase letter, one UPPERCASE letter, and one number.";
     }
 
     if (!/[A-Z]/.test(password)) {
-        return "Password must contain at least one uppercase letter.";
+        return "Password must be at least 8 characters long. At least one lowercase letter, one UPPERCASE letter, and one number.";
     }
 
     if (!/[0-9]/.test(password)) {
-        return "Password must contain at least one number.";
+        return "Password must be at least 8 characters long. At least one lowercase letter, one UPPERCASE letter, and one number.";
     }
 
     return "";
